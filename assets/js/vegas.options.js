f7d954daaa2c224404c92ead91ec9729b0b90e93
Diwas KC <diwaskc12@gmail.com>
2019-08-12 18:17:24 +0545

    navbar,slider,body almost complete

---
+++ assets/js/vegas.options.js
@@ -0,0 +1,24 @@
+
+
+$('.vh-100-header').vegas({
+    overlay: 'assets/js/overlays/08.png',
+    transition: 'fade', 
+    transitionDuration: 4000,
+    delay: 10000,
+    color: 'red',
+    animation: 'random',
+    animationDuration: 20000,
+    slides: [
+      { src: 'https://www.teachfornepal.org/media/images/da996cea-7cf1-4165-afd0-7b9020734e9d.jpeg' },
+      { src: 'http://desjacobs.com/wp-content/uploads/2015/09/IMG_2639.jpg' },
+    ]
+  });
+  
+
+  $('.highlight-slider').owlCarousel({
+    loop: true,
+    autoplay: true,
+    nav:false,
+    items: 1,
+    nav:true,
+})
